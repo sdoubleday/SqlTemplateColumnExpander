4b472bbe3fbab2bfd30915636b4da0c55a717d02
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-05-22 12:37:45 -0400

    update assemblyinfo

--- SqlTemplateColumnExpander/Properties/AssemblyInfo.cs
+++ SqlTemplateColumnExpander/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SqlTemplateColumnExpander")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("sdoubleday")]
 [assembly: AssemblyProduct("SqlTemplateColumnExpander")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
