5d264a5bf26849d2843e52832a8210e5291c95e5
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-05-22 12:35:09 -0400

    import cs project and nunit tests

---
+++ SqlTemplateColumnExpander/CommentTagElements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTemplateColumnExpander
+{
+    public class CommentTagElements
+    {
+        public String Tag { get; set; }
+        public String Pattern { get; set; }
+        public String JoinString { get; set; }
+    }
+}
---
+++ SqlTemplateColumnExpander/LineProcessorConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTemplateColumnExpander
+{
+    public class LineProcessorConfig
+    {
+        public LineProcessorConfig() { }
+        public LineProcessorConfig(string targetTag, List<string> perLineSubstitutions)
+        {
+            this.targetTag = targetTag;
+            this.perLineSubstitutions = perLineSubstitutions;
+        }
+
+        public String targetTag { get; set; }
+        public List<String> perLineSubstitutions { get; set; }
+
+
+
+    }
+}
