6beea5cccc5ceab7e97fff70b25f17eeefa36cc7
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-08-15 18:52:32 -0400

    new class componentsofColumnName. Close #30

---
+++ SqlTemplateColumnExpander/ComponentsOfColumnName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTemplateColumnExpander
+{
+    public class ComponentsOfColumnName
+    {
+
+        #region Properties
+        public List<String> ListOfColumnNameComponents { get; set; }
+        #endregion Properties
+        #region Constructors
+        public ComponentsOfColumnName(List<string> listOfColumnNameComponents)
+        {
+            this.ListOfColumnNameComponents = listOfColumnNameComponents;
+        }
+        public ComponentsOfColumnName(string OneColumnNameComponent)
+        {
+            List<string> listOfColumnNameComponents = new List<string> { OneColumnNameComponent };
+            this.ListOfColumnNameComponents = listOfColumnNameComponents;
+        }
+        #endregion Constructors
+    }
+}
